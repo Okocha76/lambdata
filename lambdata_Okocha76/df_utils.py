dec2719f02be4c1852f6061fe13d0a06b5c96e2c
Okocha76 <Okocha76@users.noreply.github.com>
2020-01-15 17:12:01 +0200

    add class to df_utils

--- lambdata_Okocha76/df_utils.py
+++ lambdata_Okocha76/df_utils.py
@@ -7,17 +7,29 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 
+class FullName:
+    '''
+    Gives a full name.
+    '''
+    def __init__(self, first, middle, last):
+        self.first = John
+        self.middle = Fitzgerald
+        self.last = Kennedy
+    
+    def what_name(self):
+        print(f'My full name is {self.first} {self.middle} {self.last}.')
+
 
 def confusion_plot(y_true, y_pred, cmap='viridis'):
     """
-    Plots a confusion matrix using Seaborn
+    Plots a confusion matrix using Seaborn.
     """
     table = pd.DataFrame(confusion_matrix(y_true, y_pred))
     return sns.heatmap(table, annot=True, fmt='d', cmap=cmap)
 
 def three_way_split(X, train_size=0.8):
     '''
-    Perform train, test, val split
+    Perform train, test, val split.
     '''
     train, test = train_test_split(X)
     train, val = train_test_split(train)
